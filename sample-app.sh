876d58fdcf6bb33ddc8a2c971e10dac82b400bfe
Soham Salkar <63644015+sohamsalkar@users.noreply.github.com>
2022-04-27 17:38:01 +0000

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -19,4 +19,4 @@ echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
-docker ps -a ssss
+docker ps -a 
